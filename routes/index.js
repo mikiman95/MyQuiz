6b318e38a36bcd559e911a59de20ed08e7f2fd54
mikiman95 <mikiman95@gmail.com>
2016-04-25 19:46:26 +0200

    5: Marco de Applicacion con Express-Partials

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,6 @@ var quizController= require('../controllers/MyQuiz_Controller');
 
 
 
-router.get('/question2', function(req, res, next) {
-  res.render('index', { title: 'detecting /question' });
-});
-
 /*GET question*/
 router.get("/question",quizController.question);
 
@@ -18,7 +14,7 @@ router.get("/check",quizController.check);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MyQuiz' });
+  res.render('index');
 });
 
 
