11754dbc08e91f4529fcf0b5297e0c74342b9d2a
mikiman95 <mikiman95@gmail.com>
2016-04-25 18:39:40 +0200

    3: Primera Pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var quizController= require('../controllers/MyQuiz_Controller');
+
+
+
+
+
+router.get('/question2', function(req, res, next) {
+  res.render('index', { title: 'detecting /question' });
+});
+
+/*GET question*/
+router.get("/question",quizController.question);
+
+/*GET Check*/
+router.get("/check",quizController.check);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MyQuiz' });
 });
+
+
 
 module.exports = router;
