054e75739a55278417a4f5b34ba9d6217f53456d
mikiman95 <mikiman95@gmail.com>
2016-04-28 17:41:20 +0200

    Tema9: Bases de Datos en Heroku

--- models/index.js
+++ models/index.js
@@ -3,12 +3,31 @@ var path = require("path");
 //Cargar el modelo ORM;
 var Sequelize = require("sequelize");  //ojo con mayusculas=constructor, minusculas=otra cosa
 
-//usar la Base de Datos SQlite:
-var sequelize = new Sequelize(
-							null,
-							null,
-							null,
-							{dialect:"sqlite",storage:"quiz.sqlite"}
+
+/*
+	Usando Entorno de Desarrollo local: BBDD con SQlilte
+		DATABASE_URL = squlite:///
+		DATABASE_STORAGE= quiz.sqlite (fichero con la BBDD)
+	Usando Entorno de Produccion en Heroku: BBDD con Postgress
+		DATABASE_URL = postgres://user:passwd@host:port/database@host:port/database
+ 		DATABASE_STORAGE no se utiliza en Heroku
+*/
+var url, storage;
+if(!process.env.DATABASE_URL){
+	url = "sqlite:///";
+	storage="quiz.sqlite";
+}else{
+	url = process.env.DATABASE_URL;
+	storage= process.env.DATABASE_STORAGE | "";
+}
+
+
+//usar la Base de Datos
+var sequelize = new Sequelize(url,
+							{
+								storage:storage,
+								omitNull:true
+							}
 							);
 
 //Importar la definicion de la tabla !uiz de quiz.js
