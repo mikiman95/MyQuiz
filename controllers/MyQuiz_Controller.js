3eb041214a0406f7516a82c5dac7e06c0ef304c2
mikiman95 <mikiman95@gmail.com>
2016-05-09 21:19:54 +0200

    Tema 14 Validacion de Pregunta y Respuesta en Create Question

--- controllers/MyQuiz_Controller.js
+++ controllers/MyQuiz_Controller.js
@@ -1,5 +1,5 @@
 var models = require("../models");
-
+var Sequelize = require("sequelize");
 
 
 
@@ -69,7 +69,7 @@ exports.new=function(req,res,next){
 
 //POST /quizzes/create
 exports.create=function(req,res,next){
-	var quiz = models.Quiz.build({question:req.body.quiz.question,answer:req.body.quiz.question});
+	var quiz = models.Quiz.build({question:req.body.quiz.question,answer:req.body.quiz.answer});
 
 	//Guardarlo en la Base de Datos;
 	quiz.save({fields:["question","answer"]})
@@ -77,6 +77,15 @@ exports.create=function(req,res,next){
 			req.flash("success","Quiz creado con Éxito.")
 			res.redirect("/quizzes");		//Si exito, servidor dice al cliente "Ahora pideme un get /quizzes"
 		})
+		.catch(Sequelize.ValidationError,function(error){
+				req.flash("error","Errores en el formulario:");
+
+				for(var i in error.errors){
+					req.flash("error",error.errors[i].value);
+				};
+				res.render("quizzes/new",{quiz:quiz});
+		})
+
 		.catch(function(error){
 			req.flash("error","Error al crear un Quiz: "+error.message);
 			next(error);
